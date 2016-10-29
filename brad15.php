b3408be7fdbc4d65c611cd8a0ec174cdb2f55974
Brad Chao <brad@brad.tw>
2016-10-29 13:34:06 +0800

    brad15 v2

--- brad15.php
+++ brad15.php
@@ -1,9 +1,9 @@
 <?php
     $p = [1 => 0,0,0,0,0,0]; // $p[1] = 0, $p[2] = 0.... $p[6] = 0;
 
-    for ($i=0; $i<100; $i++){
-        $point = rand(1,6);
-        $p[$point]++;
+    for ($i=0; $i<100000; $i++){
+        $point = rand(1,9);
+        $p[$point>6?$point-3:$point]++;
     }
 
     for ($i = 1; $i<= count($p); $i++){
