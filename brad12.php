2856fc0b276a68d1fa8cc7e95e21462381303552
Brad Chao <brad@brad.tw>
2016-10-29 12:00:10 +0800

    brad12

---
+++ brad12.php
@@ -0,0 +1,28 @@
+<?php
+    // Array
+    $a[0] = 12;
+    $a[1] = 'Brad';
+    $a[2] = true;
+
+    var_dump($a);
+    echo '<hr />';
+
+    $b[] = 3; $b[] = 123; $b[] = 'III';
+    var_dump($b);
+    echo '<hr />';
+
+    $c = [12,34,56,78];
+    var_dump($c);
+    echo '<hr />';
+
+    $d = array(11,22,33,44);
+    var_dump($d);
+    echo '<hr />';
+
+
+
+?>
+
+
+
+
