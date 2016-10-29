babae51e886faddaceef4dc78d0ba318ddcc7f89
Brad Chao <brad@brad.tw>
2016-10-29 15:58:02 +0800

    poker v4.2

--- pokerv4.php
+++ pokerv4.php
@@ -14,10 +14,16 @@
 ?>
 <table width="100%" border="1">
 <?php
+    $suit = ['<font size="6">&spades;</font>',
+            '<font size="6" color="red">&hearts;</font>',
+            '<font size="6" color="red">&diams;</font>',
+            '<font size="6">&clubs;</font>'];
+    $value = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
     foreach ($players as $player){
+        sort($player);
         echo '<tr>';
         foreach ($player as $card){
-            echo "<td>{$card}</td>";
+            echo '<td>' . $suit[(int)($card/13)] .  $value[$card%13] . '</td>';
         }
         echo '</tr>';
     }
