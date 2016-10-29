64f3623b078e5978c8e0887009e1a226cbb6d4e8
Brad Chao <brad@brad.tw>
2016-10-29 11:43:39 +0800

    brad11 v2

--- brad11.php
+++ brad11.php
@@ -1,5 +1,17 @@
+<?php
+    $n = $sum = '';
+    if (isset($_GET['n'])){
+        $n = $_GET['n'];
+        $i = $sum = 0;
+        while ($i<=$n){
+            $sum += $i++;
+        }
+    }
+
+?>
 <form>
     1 + 2 + ... +
-    <input name='n' />
+    <input name='n' value="<?php echo $n; ?>"/>
     <input type='submit' value="=" />
+    <?php echo $sum; ?>
 </form>
