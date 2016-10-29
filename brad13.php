53123718cf0e0079b1cec101170af2f6a239c78f
Brad Chao <brad@brad.tw>
2016-10-29 12:06:45 +0800

    brad13

---
+++ brad13.php
@@ -0,0 +1,20 @@
+<?php
+
+    $a[12] = 'Brad';
+    $a[1] = 'OK';
+    $a[123] = 456;
+    var_dump($a);
+    echo '<hr />';
+
+    $b['name'] = 'Brad';
+    $b['age'] = 50;
+    $b['weight'] = 80;
+    var_dump($b);
+    echo '<hr />';
+
+    $c = [1 => 123, 'name' => 'peter', 34, 78];
+    var_dump($c);
+    echo '<hr /   >';
+
+
+?>
